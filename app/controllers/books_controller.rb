31afe82de6849b6a288e775f774089593d3b4e91
akki1027 <akki10271995@gmail.com>
2020-02-24 03:08:05 +0000

    [Change] books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params:id)
   end
 
   def new
+  	@book = Book.new
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+  def update
+  	book = Book.find(patams[:id])
+  	if book.update(params[:id])
+  		flash[:update] = "Book was successfully updated."
+  		redirect_to book_path(book)
+  	else
+  		@books = Book.all
+  		render 'index'
+  	end
+  end
+
+  	private
+  	def book_params
+  	params.require(:book).permit(:title, :body)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # sign-inした際にindex画面に移動する
+  root 'books#index'
 end
