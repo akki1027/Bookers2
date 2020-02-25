e7c11e6190f287941cf6d2e7ea5da46bccf2a0cd
akki1027 <akki10271995@gmail.com>
2020-02-25 06:45:21 +0000

    [Create] PostImage model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_083114) do
+ActiveRecord::Schema.define(version: 2020_02_25_064217) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_02_24_083114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
