f602ccb9528ad0183afb21924f912465d06f27b8
akki1027 <akki10271995@gmail.com>
2020-02-25 07:16:45 +0000

    [Create] PostImages controller

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,7 @@
+class PostImagesController < ApplicationController
+	def new
+	end
+
+	def create
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users
+  resources :post_images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # sign-inした際にindex画面に移動する
   root 'books#index'
