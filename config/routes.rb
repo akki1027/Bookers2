c2d07b7be61a5794e082c700e5daf60df77eac26
akki1027 <akki10271995@gmail.com>
2020-02-26 07:35:24 +0000

    [Change] homes/top

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   resources :users
   resources :post_images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # sign-inした際にindex画面に移動する
-  root 'books#index'
 end
