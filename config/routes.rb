34d1ff66edeabb7b46d2e009c64603005a484545
akki1027 <akki10271995@gmail.com>
2020-02-26 10:31:27 +0000

    [Create] about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
+  get 'about' => 'homes#about'
   root "homes#top"
   devise_for :users
   resources :books
   resources :users
   resources :post_images
+  resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
