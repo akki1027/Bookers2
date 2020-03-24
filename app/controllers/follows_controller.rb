f4de5a3e2f199523a8f15c2d28e9cf55e8d40800
akki1027 <akki10271995@gmail.com>
2020-03-24 06:25:05 +0000

    Follow feature 非同期

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,14 +2,14 @@ class FollowsController < ApplicationController
 	def create
     @user = User.find(params[:follow][:followed_id])
     current_user.follow!(@user)
-    redirect_to request.referrer || root_path
+    # redirect_to request.referrer || root_path
   end
 
   def destroy
     @follow = Follow.find(params[:id])
     @user = User.find(@follow.followed_id)
     current_user.unfollow!(@user)
-    redirect_to request.referrer || root_path
+    # redirect_to request.referrer || root_path
   end
 end
 
